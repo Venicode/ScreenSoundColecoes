fb866fe77474184f63f1643734812b8923cb047d
Venicode <vitoria_aparecida2011@hotmail.com>
2023-11-04 11:20:37 -0300

    commit

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musicas = new Musica();
+musicas.nome = "Roxane";
+musicas.artista = "The Police";
+
+Console.WriteLine(musicas.nome);
+Console.WriteLine(musicas.artista);
+
+Musica musicas2 = new Musica();
+musicas2.nome = "Vertigo";
+musicas2.artista = "U2";
+Console.WriteLine(musicas2.nome);
+Console.WriteLine(musicas2.artista);
