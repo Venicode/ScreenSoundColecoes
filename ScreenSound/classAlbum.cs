4d92b7deba37fc0bb9ff3617e74b0ea5da955f3b
Veni <44931124+Venicode@users.noreply.github.com>
2023-11-11 12:42:24 -0300

    Update classAlbum.cs

--- ScreenSound/classAlbum.cs
+++ ScreenSound/classAlbum.cs
@@ -1,21 +1,28 @@
-﻿class Album
+class Album
 {
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+
     private List<Musica> musicas = new();
-    public string Nome { get; set; }
-    public string Genero { get; set; }
-    public double DuracaoTotal => musicas.Sum(tempo => tempo.Duracao/60);
+
+    public string Nome { get; }
+    public int DuracaoTotal => musicas.Sum(tempo => tempo.Duracao);
     //Metodo para adicionar música usando como parametro a
     //variavel musica da classe Musica
-    public void AdicionarMusica (Musica musica)
+    public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
     }
 
-    public void ExibirMusicasAlbum() {
+    public void ExibirMusicasAlbum()
+    {
         Console.WriteLine($"Lista de músicas do álbum {Nome}");
-        foreach (var musica in musicas) {
+        foreach (var musica in musicas)
+        {
             Console.WriteLine($"Música: {musica.Nome}");
         }
-        
+
     }
 }
